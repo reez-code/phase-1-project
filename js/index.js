a709b9a8b6d4ca2a7953e73c170fd498a71d9cfa
Phil <phil.github@gmail.com>
2024-04-12 16:46:06 +0300

    added extra news section to website

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ fetch(BASE_URL, requestOptions)
   .then((response) => response.json())
   .then((result) => {
     result.articles.forEach((items) => {
-      console.log(items);
+      moreNews(items);
     });
   })
   .catch((error) => console.error(error));
@@ -44,10 +44,16 @@ function secondSetofCards(articles) {
   // accesses the second content and creates cards
   const selectDiv = document.querySelector("#list");
   const secondSet = document.createElement("div");
-  secondSet.className = "col";
+  secondSet.className = "card mb-3";
   secondSet.innerHTML = `<img src=${articles.urlToImage} class=card-img-top alt=.../> <div class=card-body><h5 class=card-title id=second>${articles.title}</h5><p class=card-text>${articles.description}</p><p>For More Info Visit: <a href=${articles.url} target="_blank">${articles.source["name"]}</a></p><p class=card-text><small class=text-body-secondary>Last updated 3 mins ago</small></p></div>`;
   // secondSet.innerHTML = `<div class=col><div class=card><img src=${articles.urlToImage} class=card-img-top alt=.../> <div class=card-body><h5 class=card-title id=second>${articles.title}</h5><p class=card-text>${articles.description}</p><p>For More Info Visit: <a href=${articles.url} target="_blank">${articles.source["name"]}</a></p></div></div></div>`;
   selectDiv.append(secondSet);
 }
 
-function moreNews() {}
+function moreNews(news) {
+  const selectDiv = document.querySelector("#more-news");
+  const addCards = document.createElement("div");
+  addCards.className = "col";
+  addCards.innerHTML = `<div class=card><img src=${news.urlToImage} class=card-img-top alt=.../> <div class=card-body><h5 class=card-title id=second>${news.title}</h5><p class=card-text>${news.description}</p><p>For More Info Visit: <a href=${news.url} target="_blank">${news.source["name"]}</a></p></div></div>`;
+  selectDiv.append(addCards);
+}
